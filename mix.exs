122ebecd5811b6c4f4731510e7985c828658e93e
Paolo D'Incau <paolo.dincau@xpeppers.com>
2015-07-28 21:11:31 +0200

    removing dependency

--- mix.exs
+++ mix.exs
@@ -34,7 +34,6 @@ defmodule Cromer.Mixfile do
      {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 1.1"},
      {:phoenix_live_reload, "~> 0.4", only: :dev},
-     {:cowboy, "~> 1.0"},
-     {:comeonin, "~> 1.0"}]
+     {:cowboy, "~> 1.0"}]
   end
 end
